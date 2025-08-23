2e325210bf038fd8d9995dc774d02786a19bb010
Vinicius Inocêncio de Souza <vicnicius2009@gmail.com>
2025-08-22 22:16:06 -0300

    chore: creating pwa configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png", "maskable-icon-512x512.png"],
+      manifest: {
+        name: "Numenera Character Generator",
+        short_name: "Numenera Char Gen",
+        description:
+          "This is a character generator for the Numenera tabletop RPG.",
+        theme_color: "#ffffff",
+        icons: [
+        {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        prefer_related_applications: false,
+      },
+    }),
+  ],
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
 });
